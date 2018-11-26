bfb480428baff9a1aa9874a3f93b7035f57a4ff0
Chris van Riemsdijk <610481@owgn.nl>
2018-11-26 10:27:18 +0100

    Compare.py changes

--- scripts/compare.py
+++ scripts/compare.py
@@ -5,11 +5,21 @@ import matplotlib.pyplot as plt
 path_to_file = "../supdata/supdata_f_metadata.csv"
 data = pd.read_csv(path_to_file, encoding='utf-8')
 
+#sort de data zodat de N/A waarden worden gedropt (Houdt alleen cancer over.)
+sorted_data = data.dropna()
+
+mean_age = sorted_data.Age.mean()
+print(mean_age)
+mean_BMI = sorted_data.BMI.mean()
+print(mean_BMI)
+mean_TNM = sorted_data.TNM.value_counts()
+print(mean_TNM)
+
+
+
+
+
 # a = data.groupby('Diagnosis').Age.mean()
-# b = data[data.Diagnosis == ['Cancer']
+# b = data[data.Diagnosis == ['Cancer']]
 # c = data.groupby('Diagnosis')
-
-
-# print(a)
-# print(b)
-# print(c)
+# d = data.dtypes
