16837f1178b967933b15c509c0b264d82a47bbd5
riemsdijkCS <chrisvanriemsdijk@gmail.com>
2019-02-05 12:31:21 +0100

    T-test working on whole df

--- final/t-test.py
+++ final/t-test.py
@@ -7,8 +7,17 @@ df = pd.read_csv('merged.csv')
 # a = stats.ttest_ind(df.Methanoculleus_marisnigri[df.Diagnosis == 'Cancer'][df.Diagnosis == "Small Adenoma"],df.Methanoculleus_marisnigri[df.Diagnosis == 'Normal'])
 
 for column in df:
-    p = stats.ttest_ind(df.loc[df['Diagnosis'] =='Cancer' | df['Diagnosis'] == 'Small adenoma', column], df.loc[df['Diagnosis'] == 'Normal',column])
-    print('kolom', column, ': p=', p)
+    try:
+        p = stats.ttest_ind(df.loc[df['Diagnosis'] == 'Normal',column],df.loc[(df['Diagnosis'] == 'Cancer') | (df['Diagnosis'] == 'Small Adenoma'), column])
+
+        if p.pvalue <= 0.01:
+            print("Kolom: ", column, "p= ", p.pvalue)
+        else:
+            pass
+
+    except:
+        pass
+
 
 
 
