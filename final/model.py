9becc0599a9b778d9e1e96117559568e5d368349
riemsdijkcs <chrisvanriemsdijk@gmail.com>
2019-02-19 14:34:19 +0100

    Modified model.py with model_2

--- final/model.py
+++ final/model.py
@@ -1,24 +1,36 @@
 import pandas as pd 
 from sklearn.model_selection import LeaveOneOut
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-data = pd.read_csv('model.csv')
+data = pd.read_csv('model_2.csv')
+
+#Probleem waar ik tegen aan loop, hoe geef ik mijn target en data met pandas
+X = data.iloc[:,0:11]
+y = data.loc[:,'Diagnosis']
 
 loo = LeaveOneOut()
 
-for train_index, test_index in loo.split(data):
+print(X)
+print(y)
+
+
+
+
+
+for train_index, test_index in loo.split(X): # Split in X
     print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = data[train_index], data[test_index]
-    y_train, y_test = data[train_index], data[test_index]
-    print(X_train, X_test, y_train, y_test)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 
-model = LogisticRegression()
+# model = LogisticRegression()
 
-model.fit(X_train, y_train)
+# model.fit(X_train, y_train)
+# z = model.score(X_test, y_test)
 
-
-
+# print(z)
 
 
 
--- scripts/convert.py
+++ scripts/convert.py
@@ -1,5 +1,5 @@
 import pandas as pd 
 
 #Converteert de excelsheet naar een CSV voor makkelijke data vergelijkingen
-df = pd.read_excel('../final/model.xlsx')  # sheetname is optional
-df.to_csv('model.csv', index=False)  # index=False prevents pandas to write row index
+df = pd.read_excel('../model_2.xlsx')  # sheetname is optional
+df.to_csv('model_2.csv', index=False)  # index=False prevents pandas to write row index
